257daa87c3627540e8a65b59e5f3f65ed4ffb640
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-03-24 07:46:00 -0300

    Adicionar arquivos de projeto.

---
+++ ClasseTriangulo/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace ClasseTriangulo {
+    public class Triangulo {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public Triangulo(double a, double b, double c) { 
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double AreaT() {
+            double area = (B * A) / 2;
+
+            return area;
+        }
+
+        public override string ToString() {
+            return "\nLado A: " + A + "\nLado B: " + B + "\nLado C: " + C;
+        }
+    }
+}
---
+++ triangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClasseTriangulo;
+
+internal class Program {
+    private static void Main(string[] args) {
+        /* Crie uma abstração triangulo
+         * emparelhe suas propriedades para
+         que só possam ser lidas e não ateradas
+        
+         crie um metodo que retorne
+        a area do triangulo
+        
+         dica -> um triangulo não existe sem os 3 lados.
+        
+         Preencha os lados do contrutor.*/
+
+        double[] lado = new double[3];
+
+        for(int i = 0; i < 3; i++) {
+            Console.WriteLine("Digite um lado do triangulo");
+            lado[i] = double.Parse(Console.ReadLine());
+
+            if (lado[i] == 0) {
+                i = 1 - 1;
+                Console.WriteLine("Numero invalido, digite novamente: ");
+                lado[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        Triangulo tri = new Triangulo(lado[0], lado[1], lado[2]);
+        double area = tri.AreaT();
+
+        Console.WriteLine("Os lados do Triangulo são: " + tri);
+        Console.WriteLine("\nA area do seu triangulo é: " + area);
+
+    }
+}
